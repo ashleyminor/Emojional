31674b6c9fd46a5afcba17766306ad22c1c1bb3a
Ashley Minor <ashleyminor07@gmail.com>
2019-05-12 18:33:00 -0500

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)}
 
 }
 
