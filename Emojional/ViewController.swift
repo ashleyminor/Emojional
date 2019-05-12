2f8c062ef6dedcf222abdf952a9c1e89a40128a0
Ashley Minor <ashleyminor07@gmail.com>
2019-05-12 17:32:56 -0500

    Complete Iteration 0

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Ashley Minor on 5/12/19.
+//  Copyright © 2019 Ashley Minor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
